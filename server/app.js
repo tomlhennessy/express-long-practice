96f18a1f84d0d522a247e82e3b6bcaa9388a2304
Tom Hennessy <tomlhennessy@outlook.com>
2024-11-25 15:13:00 +1000

    handled unmatched routes, logged status codes and request details

--- server/app.js
+++ server/app.js
@@ -8,6 +8,20 @@ app.use(express.json()); // for JSON parsing
 // serve static files under /static
 app.use('/static', express.static(path.join(__dirname, 'assets')));
 
+// logger middleware
+const loggerMiddleware = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`); // log method and URL
+
+  res.on('finish', () => {
+    console.log(`Status Code: ${res.statusCode}`); // log status code
+  })
+
+
+  next(); // pass to the next middleware or route handler
+}
+// add logger middleware
+app.use(loggerMiddleware);
+
 // For testing purposes, GET /
 app.get('/', (req, res) => {
   res.json("Express server running. No content provided at root level. Please use another route.");
@@ -26,11 +40,19 @@ app.get('/test-error', async (req, res) => {
   throw new Error("Hello World!")
 });
 
+// resource not found middleware
+app.use((req, res, next) => {
+  const err = new Error("The requested resource couldn't be found.");
+  err.statusCode = 404;
+  next(err); // pass to the error handler
+})
+
+// Custom Error Handler Middleware
 app.use((err, req, res, next) => {
   res.status(err.statusCode || 500).json({
     error: err.message || 'Internal Server Error',
-  })
-})
+  });
+});
 
 const port = 5001;
 app.listen(port, () => console.log('Server is listening on port', port));
